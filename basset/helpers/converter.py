a5cacf4fb1c4213393a380b0c109a544322b3b27
Michal Mizera <axadiw@gmail.com>
2015-06-08 18:22:51 +0200

    Correctly handling filenames without extension

--- basset/helpers/converter.py
+++ basset/helpers/converter.py
@@ -24,15 +24,16 @@ class Converter:
         if not os.path.isdir(self.inputDir):
             for path, subdirectories, files in os.walk(os.getcwd()):
                 path = os.path.relpath(path, os.getcwd())
-                for original_filename in files:
-                    extension = original_filename.split(".")[1]
+                for filename in files:
+                    if "." in filename:
+                        extension = filename.split(".")[1]
 
-                    if extension.lower() in allowed_image_types:
-                        top_dir_in_path = path.split(os.sep)[0]
-                        if top_dir_in_path in directories_with_vector_files:
-                            directories_with_vector_files[top_dir_in_path] += 1
-                        else:
-                            directories_with_vector_files[top_dir_in_path] = 1
+                        if extension.lower() in allowed_image_types:
+                            top_dir_in_path = path.split(os.sep)[0]
+                            if top_dir_in_path in directories_with_vector_files:
+                                directories_with_vector_files[top_dir_in_path] += 1
+                            else:
+                                directories_with_vector_files[top_dir_in_path] = 1
 
             max_vector_files_count = -1
             directory_with_max_vector_files = None
@@ -50,34 +51,35 @@ class Converter:
         converted_files_count = 0
 
         for original_base_path, subdirectories, files in os.walk(self.inputDir):
-            for original_filename in files:
-                basename = original_filename.split(".")[0]
-                extension = original_filename.split(".")[1]
+            for filename in files:
+                if "." in filename:
+                    basename = filename.split(".")[0]
+                    extension = filename.split(".")[1]
 
-                if extension.lower() in allowed_image_types:
-                    new_base_path = original_base_path.replace(self.inputDir, self.outputDir)
-                    if not os.path.exists(new_base_path):
-                        os.makedirs(new_base_path)
-                    original_full_path = os.path.join(original_base_path, original_filename)
+                    if extension.lower() in allowed_image_types:
+                        new_base_path = original_base_path.replace(self.inputDir, self.outputDir)
+                        if not os.path.exists(new_base_path):
+                            os.makedirs(new_base_path)
+                        original_full_path = os.path.join(original_base_path, filename)
 
-                    logging.info("Converting " + original_full_path)
-                    converted_files_count += 1
+                        logging.info("Converting " + original_full_path)
+                        converted_files_count += 1
 
-                    with Image(filename=original_full_path) as img:
-                        original_size = img.size
+                        with Image(filename=original_full_path) as img:
+                            original_size = img.size
 
-                    image_size_1x = (original_size[0], original_size[0])
-                    image_size_2x = (original_size[0] * 2, original_size[0] * 2)
-                    image_size_3x = (original_size[0] * 3, original_size[0] * 3)
-                    with Image(filename=original_full_path, resolution=image_size_1x) as img:
-                        img.resize(*image_size_1x)
-                        img.save(filename=os.path.join(new_base_path, basename + ".png"))
-                    with Image(filename=original_full_path, resolution=image_size_2x) as img:
-                        img.resize(*image_size_2x)
-                        img.save(filename=os.path.join(new_base_path, basename + "@2x.png"))
-                    with Image(filename=original_full_path, resolution=image_size_3x) as img:
-                        img.resize(*image_size_3x)
-                        img.save(filename=os.path.join(new_base_path, basename + "@3x.png"))
+                        image_size_1x = (original_size[0], original_size[0])
+                        image_size_2x = (original_size[0] * 2, original_size[0] * 2)
+                        image_size_3x = (original_size[0] * 3, original_size[0] * 3)
+                        with Image(filename=original_full_path, resolution=image_size_1x) as img:
+                            img.resize(*image_size_1x)
+                            img.save(filename=os.path.join(new_base_path, basename + ".png"))
+                        with Image(filename=original_full_path, resolution=image_size_2x) as img:
+                            img.resize(*image_size_2x)
+                            img.save(filename=os.path.join(new_base_path, basename + "@2x.png"))
+                        with Image(filename=original_full_path, resolution=image_size_3x) as img:
+                            img.resize(*image_size_3x)
+                            img.save(filename=os.path.join(new_base_path, basename + "@3x.png"))
 
         logging.info("Images conversion finished. Converted " + str(converted_files_count) + " images")
 
